57c5999235e9314f71d2c3e3c5069946e2b35de6
Júlio Pattuzzo <pattuzzo@protonmail.com>
2024-03-23 18:08:33 -0300

    updated github link

--- src/layout/footer/index.tsx
+++ src/layout/footer/index.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer class="relative h-[5vh] w-full flex justify-center items-center px-8 bg-gray-300 dark:bg-gray-800">
       © 2024 Nexon Utilis. All rights reserved.
-      <a class="absolute top-1/2 right-5 -translate-y-1/2" href="https://github.com/pattuzzoj"><Icon name="BsGithub" class="size-5" /></a>
+      <a class="absolute top-1/2 right-5 -translate-y-1/2" href="https://github.com/pattuzzoj/nexonutilis"><Icon name="BsGithub" class="size-5" /></a>
     </footer>
   );
 }
