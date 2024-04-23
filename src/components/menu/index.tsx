e63274a0d3af14715d3ceec6eba03ae1745f4647
Júlio Pattuzzo <pattuzzo@protonmail.com>
2024-04-23 17:02:43 -0300

    fix: menu category

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -6,7 +6,7 @@ export default function Menu() {
 
   return (
     <div class="h-[75vh] w-full flex flex-col items-center">
-      <div class="w-full flex flex-col gap-4 overflow-y-auto rounded-2xl p-2 bg-gray-300 dark:bg-zinc-900">
+      <div class="w-full flex flex-col gap-3 overflow-y-auto rounded-2xl p-2 bg-gray-300 dark:bg-zinc-900">
         <For each={data.data}>
           {(item: any) => <MenuItem {...item} depth={2}/>}
         </For>
--- src/components/menu/menuItems/index.tsx
+++ src/components/menu/menuItems/index.tsx
@@ -23,20 +23,19 @@ export default function MenuItem(props: MenuItemProps) {
 
   return (
     <nav class="w-full flex flex-col gap-2" aria-label={props.title} aria-expanded={isOpen() ? "true" : "false"}>
-      <Title as={String(props.depth) as "2" | "3" | "4" | "5" | "6"} style={`font-size: ${1.6 - (0.2 * props.depth) + "rem"}`} class="group">
-        <a
-        class={`${isOpen() && "bg-gray-100 dark:bg-zinc-700"} flex items-center gap-2 rounded-xl py-1 px-2 text-inherit group-hover:text-lg
-        ]] hover:bg-gray-100 dark:hover:bg-zinc-700`} href={!isOpen() ? props.url : props.url.slice(0, (props.url).lastIndexOf('/'))}
-        onClick={(e) => {
-          e.preventDefault();
-          setIsOpen(!isOpen());
-          navigate(isOpen() ? props.url : props.url.slice(0, (props.url).lastIndexOf('/')));
-        }}
-        >
-          <Icon name={props?.icon || "RiArrowsArrowRightSLine"} class={`${isOpen() && "animate-spin"} group-hover:animate-spin size-6`}/>
-          {props.title}
-        </a>
-      </Title>
+      <a
+      class={`${isOpen() && "bg-gray-100 dark:bg-zinc-700"} group flex items-center gap-2 rounded-xl py-1 px-2 text-inherit
+      ]] hover:bg-gray-100 dark:hover:bg-zinc-700`} href={!isOpen() ? props.url : props.url.slice(0, (props.url).lastIndexOf('/'))}
+      style={`font-size: ${1.6 - (0.2 * props.depth) + "rem"}`}
+      onClick={(e) => {
+        e.preventDefault();
+        setIsOpen(!isOpen());
+        navigate(isOpen() ? props.url : props.url.slice(0, (props.url).lastIndexOf('/')));
+      }}
+      >
+        <Icon name={props?.icon || "RiArrowsArrowRightSLine"} class={`${isOpen() && "animate-spin"} group-hover:animate-spin size-6`}/>
+        {props.title}
+      </a>
       <div class={`${isOpen() ? "h-full" : "h-0 scale-0"} flex flex-col gap-1.5 overflow-hidden transition-all duration-300`} >
         <For each={props?.items}>
           {(item) => (
@@ -45,7 +44,7 @@ export default function MenuItem(props: MenuItemProps) {
                 <MenuItem {...item} depth={(props.depth || 2) + 1}/>
               </Match>
               <Match when={item?.type == "resource"}>
-                <a class={`${(path() == item.url) && "bg-gray-100 dark:bg-zinc-700 text-sm"} w-full py-1 px-2 text-base hover:text-sm rounded-xl hover:bg-gray-100 dark:hover:bg-zinc-700 transition-all`} href={item.url}>{item.title}</a>
+                <a class={`${(path() == item.url) && "bg-gray-100 dark:bg-zinc-700 text-sm"} w-full py-1 px-2 text-base rounded-xl hover:bg-gray-100 dark:hover:bg-zinc-700 transition-all`} href={item.url}>{item.title}</a>
               </Match>
             </Switch>
           )}
