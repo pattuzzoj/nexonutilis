f37fc40e3f559b2ab33071cc702f51571abde4cc
Júlio Pattuzzo <pattuzzo@protonmail.com>
2024-04-28 19:33:22 -0300

    fix: search function

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -3,20 +3,18 @@ import { useData } from "context";
 import { Category } from "models/interfaces/category";
 
 export default function Search() {
-  const [list, setList] = createSignal<Array<{title: string, url: string}>>([]);
+  const [list, setList] = createSignal<Array<{title: string, url: string, category: string}>>([]);
   const [value, setValue] = createSignal<string>('');
   const {data} = useData();
 
   function filterList(e: InputEvent) {
     const input = e.target as HTMLInputElement;
     setList([]);
-    
-    if(input.value != '') {
-      data.routes.forEach(({type, title, url}: Category) => {
-        if(url.lastIndexOf('/')) {
-          if(type == "resource" && title.toLowerCase().startsWith(input.value.toLowerCase())) {
-            setList(items => [...items, {"title": title, "url": url}]);
-          }
+
+    if(input.value != ``) {
+      data.routes.forEach(({title, url}: Category) => {
+        if(url?.lastIndexOf("/") && title?.split(" ").some((value) => value.toLowerCase().startsWith(input.value.toLowerCase()))) {
+          setList(items => [...items, {"title": title, "url": url, "category": data.routes.get(url.slice(0, url.indexOf("/", 1))).title.toLowerCase()}]);
         }
       })
     }
@@ -33,7 +31,11 @@ export default function Search() {
         onClick={(e) => {if(e.target.tagName == 'A') setValue(''); setList([]);}}
         >
           <For each={list()}>
-            {({title, url}) => <li><a class="block rounded-lg p-2 hover:bg-gray-100 dark:hover:bg-zinc-700" href={url}>{title}</a></li>}
+            {({title, url, category}) => (
+            <li>
+              <a class="flex justify-between rounded-lg p-2 text-sm hover:bg-gray-100 dark:hover:bg-zinc-700" href={url}>{title}<span class="rounded-lg p-1 text-xs bg-gray-100 dark:bg-zinc-700">{category}</span></a>
+              </li>
+            )}
           </For>
         </ul>
       </Show>
