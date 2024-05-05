43d56c9c9bca4931cfde102464494012249a95ef
Júlio Pattuzzo <pattuzzo@protonmail.com>
2024-05-05 09:23:22 -0300

    fix: imports category

--- src/database/engineering/index.ts
+++ src/database/engineering/index.ts
@@ -1,9 +1,7 @@
 import { Category } from 'models/category';
-import { frontend } from './architecture';
-import { backend } from './design';
 
 export const web = new Category({
   title: "Web",
   description: "Explore tutorials that cover the essentials of developing applications for mobile devices. Learn to navigate the diverse ecosystem, understand user interface best practices, and implement functionalities optimized for smartphones and tablets.",
   icon: "TbWorldCode",
-}, [frontend, backend]);
+}, []);
